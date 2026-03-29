690f66afa8dd2edf5187c70d8f4121c482ee6340
Harmeet <hh7360165@gmail.com>
2026-03-29 17:14:17 +0530

    changed audio and added the more timer options

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import { FaMinus, FaPlus } from "react-icons/fa";
 import { useState, useEffect } from 'react';
-import tickingSound from '../../assets/tick.mp3'
-import timeup from '../../assets/timeup.mp3'
+import tickingSound from '../../assets/kbc.mp3'
+import timeup from '../../assets/buzzer.mp3'
+
+const ticking = new Audio(tickingSound)
+const buzz = new Audio(timeup);
 
 const Home = () => {
 
     const [isRunning, setIsrunning] = useState(false);
     const [time, setTime] = useState(0);
     const [flash, setFlash] = useState({});
-    const ticking = new Audio(tickingSound)
-    const buzz  = new Audio(timeup);
-
 
     const defaultTeams = [
         { id: 1, team: 'Jal', score: 0 },
@@ -35,6 +35,7 @@ const Home = () => {
     useEffect(() => { if (!isRunning) { ticking.pause(); ticking.currentTime = 0; } }, [isRunning])
 
 
+
     useEffect(() => {
         if (!isRunning) return;
 
@@ -49,13 +50,13 @@ const Home = () => {
                 }
 
                 if (prev > 1) {
-                    ticking.currentTime = 0;
+                    // ticking.currentTime = 0;
                     ticking.play().catch((e) => console.warn("Audio blocked:", e));
                 } else {
                     buzz.currentTime = 0;
                     buzz.play()
-                    ticking.pause();
                     ticking.currentTime = 0;
+                    ticking.pause();
                 }
 
                 return prev - 1;
@@ -99,49 +100,58 @@ const Home = () => {
     }, [teams])
 
 
+    const resetTimer = () => {
+        setTime(0);
+        ticking.currentTime = 0;
+        ticking.pause();
+        setIsrunning(false);
+    }
+
+
     return (
-        <div style={{ fontFamily: 'poppins' }} className='flex flex-col min-h-[100vh] bg-blue-200'>
-            <div className='flex uppercase items-center mt-7 justify-center'>
-                <p className='text-black/80 text-5xl font-bold'>Scoreboard</p>
-            </div>
-
-            <div className='flex flex-row gap-30 mt-20 justify-center'>
-
-                <div className='grid grid-cols-2 gap-5'>
-                    {teams.map((v) => (
-                        <div
-                            key={v.id}
-                            className={`flex p-4 font-[500] min-w-[400px] rounded-xl shadow-lg flex-col transition-all duration-300
-                                ${flash[v.id] === "plus"
-                                    ? "bg-green-400"
-                                    : flash[v.id] === "minus"
-                                        ? "bg-red-400"
-                                        : "bg-white"
-                                }
-                            `}>
-                            <div className='flex items-center text-3xl justify-between'>
-                                <p className='bg-blue-600 p-2 text-white rounded-lg'>Team - {v.team}</p>
-                                <div className='flex gap-2'>
-                                    <div onClick={() => handleIncrement(v.id)} className='flex cursor-pointer text-white items-center h-10 w-10 justify-center rounded-full overflow-hidden bg-black/80'><FaPlus className='text-xl' /></div>
-                                    <div onClick={() => handleDecrement(v.id)} className='flex cursor-pointer text-white items-center h-10 w-10 justify-center rounded-full overflow-hidden bg-black/80'><FaMinus className='text-xl' /></div>
-                                </div>
-                            </div>
-                            <div className='text-3xl mt-3'><p>Score: {v.score}</p></div>
-                        </div>
-                    ))}
-
-
+        <>
+            <div style={{ fontFamily: 'poppins' }} className='flex pt-10 z-4 relative bg-blue-200 flex-col min-h-[100vh]'>
+                <div className='flex uppercase items-center mt-7 justify-center'>
+                    <p className='text-black/80 text-5xl font-bold'>Scoreboard</p>
                 </div>
 
+                <div className='flex flex-row gap-30 mt-20 justify-center'>
 
-                <div className='flex flex-col'>
-                    <div className='flex text-4xl items-center gap-2 font-semibold flex-col'>
-                        <p>Time Left</p>
-                        <p className='text-9xl bg-black/80 text-white p-4 rounded-lg w-fit text-center font-mono'>{`${Math.floor(time / 60)}`.padStart(2, 0)}:
-                            {`${time % 60}`.padStart(2, 0)}</p>
+                    <div className='grid grid-cols-2 gap-5'>
+                        {teams.map((v) => (
+                            <div
+                                key={v.id}
+                                className={`flex p-4 font-[500] min-w-[400px] rounded-xl shadow-lg flex-col transition-all duration-300
+                                ${flash[v.id] === "plus"
+                                        ? "bg-green-400"
+                                        : flash[v.id] === "minus"
+                                            ? "bg-red-400"
+                                            : "bg-white"
+                                    }
+                            `}>
+                                <div className='flex items-center text-3xl justify-between'>
+                                    <p className='bg-blue-600 p-2 text-white rounded-lg'>Team - {v.team}</p>
+                                    <div className='flex gap-2'>
+                                        <div onClick={() => handleIncrement(v.id)} className='flex cursor-pointer text-white items-center h-10 w-10 justify-center rounded-full overflow-hidden bg-black/80'><FaPlus className='text-xl' /></div>
+                                        <div onClick={() => handleDecrement(v.id)} className='flex cursor-pointer text-white items-center h-10 w-10 justify-center rounded-full overflow-hidden bg-black/80'><FaMinus className='text-xl' /></div>
+                                    </div>
+                                </div>
+                                <div className='text-3xl mt-3'><p>Score: {v.score}</p></div>
+                            </div>
+                        ))}
+
+
                     </div>
-                    <div className='flex flex-col items-center text-2xl mt-5 gap-3'>
-                        {/* <div>
+
+
+                    <div className='flex flex-col'>
+                        <div className='flex text-4xl items-center gap-2 font-semibold flex-col'>
+                            <p>Time Left</p>
+                            <p className='text-9xl bg-black/80 text-white p-4 rounded-lg w-fit text-center font-mono'>{`${Math.floor(time / 60)}`.padStart(2, 0)}:
+                                {`${time % 60}`.padStart(2, 0)}</p>
+                        </div>
+                        <div className='flex flex-col items-center text-2xl mt-5 gap-3'>
+                            {/* <div>
                             <select className='bg-black/80 text-white rounded-lg px-2 py-0' onChange={handleInput} value={time ? time / 60 : ""} id="">
                                 <option value="">Select Time</option>
                                 <option value="1">1 minute</option>
@@ -151,20 +161,27 @@ const Home = () => {
                                 <option value="5">5 minute</option>
                             </select>
                         </div> */}
-                        <div className='grid grid-cols-4 gap-3 mt-4'>
-                            <div onClick={() => { setTime(15); setIsrunning(true) }} className='text-2xl bg-blue-600 p-2 rounded-md text-white'>15s</div>
-                            <div onClick={() => { setTime(20); setIsrunning(true) }} className='text-2xl bg-blue-600 p-2 rounded-md text-white'>20s</div>
-                            <div onClick={() => { setTime(30); setIsrunning(true) }} className='text-2xl bg-blue-600 p-2 rounded-md text-white'>30s</div>
-                            <div onClick={() => { setTime(45); setIsrunning(true) }} className='text-2xl bg-blue-600 p-2 rounded-md text-white'>45s</div>
+                            <div className='grid grid-cols-3  gap-3 mt-4'>
+                                <div onClick={() => { setTime(15); setIsrunning(true) }} className='text-2xl bg-blue-600 p-2 rounded-md text-white'>15s</div>
+                                <div onClick={() => { setTime(20); setIsrunning(true) }} className='text-2xl bg-blue-600 p-2 rounded-md text-white'>20s</div>
+                                <div onClick={() => { setTime(30); setIsrunning(true) }} className='text-2xl bg-blue-600 p-2 rounded-md text-white'>30s</div>
+                                <div onClick={() => { setTime(45); setIsrunning(true) }} className='text-2xl bg-blue-600 p-2 rounded-md text-white'>45s</div>
+                                <div onClick={() => { setTime(60); setIsrunning(true) }} className='text-2xl bg-blue-600 p-2 rounded-md text-white'>60s</div>
+                                <div onClick={() => { setTime(90); setIsrunning(true) }} className='text-2xl bg-blue-600 p-2 rounded-md text-white'>90s</div>
 
-                        </div>
-                        <div className='flex cursor-pointer gap-2'>
-                            <div onClick={() => { setIsrunning(false); setTime(0) }} ><p className='bg-blue-600 text-white px-2 py-1 rounded-lg'>Reset</p></div>
+
+                            </div>
+                            <div className='flex cursor-pointer gap-2'>
+                                <div onClick={resetTimer} ><p className='bg-blue-600 text-white px-2 py-1 rounded-lg'>Reset</p></div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+            {/* <div className='fixed top-0 left-0 h-full w-full bg-blue-200 z-1'></div>
+        <div className='fixed z-2 left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2'><img src={logoSant} className='h-[800px] w-[800px]' alt="" /></div>
+        <div className='fixed top-0 left-0 h-full w-full backdrop-blur-md z-2'></div> */}
+        </>
     )
 }
 
